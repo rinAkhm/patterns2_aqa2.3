9ab2ac47b31c79c5bbdbcd7ff532d510938cd7f1
Rinat Akhmetzyanov <rin.akhm@gmail.com>
2020-02-14 10:15:00 +0300

    inital

---
+++ src/test/java/RegistrationAсс.java
@@ -0,0 +1,13 @@
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistrationAсс {
+    String login;
+    String password;
+    String status;
+}
